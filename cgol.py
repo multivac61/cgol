1f9bd56870cfd634c2f71ef2b2b8fd6504680ced
multivac61 <olafur@genkiinstruments.com>
2018-09-30 22:30:04 +0000

    v.02 - Implemented simple and clean class structure and removed bloat.

--- cgol.py
+++ cgol.py
@@ -1,7 +1,8 @@
 # Copyright 2013, Olafur Bogason
 # Conway's game of life.
 # http://en.wikipedia.org/wiki/Conway%27s_Game_of_Life
-# The console print is based of http://code.activestate.com/recipes/578167-position-the-cursor-almost-anywhere-inside-standar/
+# The console print is based of
+# http://code.activestate.com/recipes/578167-position-the-cursor-almost-anywhere-inside-standar/
 #
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
@@ -15,101 +16,90 @@
 # included in all copies or substantial portions of the Software.
 
 # This is a bare-bone alpha version merely coded to make a sort-of skeleton for future versions. 
-# Future vesions will most definitely have OOP and a nicer console display (maybe colour! who knows).
+# Future versions will most definitely have OOP and a nicer console display (maybe colour! who knows).
+
+# v.02 30/09/18 - OOP Implemented. Nicer console display remains missing..
 
 
 from time import sleep
-from random import uniform
-from copy import deepcopy
+import numpy as np
+from os import system, name
 
-print "Oh holy ruler, welcome to \n"
 
-print """    #####  #####  #####  ##   [v.01]
-    ##     ##     ##  #  ##
-    ##     ## ##  ##  #  ##
-    #####  #####  #####  ######
-        """
-SIZE = int(raw_input('Please input the size of The Garden of Eden [positive integer]: '))
-CHANCE_OF_LIFE = float(raw_input('Please input the chances of initial life [positive double between 0-1 ]: '))
-TIME_DELAY = float(raw_input('Please specify the time period which life should develop by [positive double]: '))
+class GameOfLife:
+    Offsets = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
 
-def rndmMtrx(size):
-    mtrx = []
-    for i in range(size):
-        mtrx.append([1 if uniform(0,1) <= CHANCE_OF_LIFE else 0 for x in range(size)])
-    return mtrx
+    def __init__(self, n, m):
+        self.N, self.M = n, m
+        self.current_grid, self.future_grid = np.zeros((n, m)), np.zeros((n, m))
 
-def clear():
-    for n in range(0, 64, 1): 
-        print("\r\n")
+    def evolve(self):
+        for row in range(self.N):
+            for col in range(self.M):
+                self.evolve_cell(row, col)
 
-def printLine(user_string = "", hor = 0, ver = 0):
-    # Fixing an error in the hor/ver starting point (somehow 0 and 1 both start in 0)
-    hor += 1
-    ver += 1
-    # Plot the user_string at the starting at position hor, ver
-    print("\033[" + str(ver) + ";" + str(hor) + "f" + formatLine(user_string)) 
+        self.current_grid = np.copy(self.future_grid)
 
-def formatLine(line):
-    output = ""
-    for i, item in enumerate(line):
-        item = str(item)
-        item = item.replace('1', 'X').replace('0', ' ')
-        if i < len(line):
-            output += item + " "
+    def randomize(self, chance):
+        self.current_grid = np.vectorize(lambda x: 1 if x > chance else 0)(np.random.rand(self.N, self.M))
+
+    def neighbours(self, i, j):
+        alive_neighbours = 0
+        for (ii, jj) in self.Offsets:
+            x, y = i + ii, j + jj
+            if 0 <= x < self.N and 0 <= y < self.M and self.current_grid[x, y] == 1:
+                alive_neighbours += 1
+
+        return alive_neighbours
+
+    def evolve_cell(self, i, j):
+        alive = (self.current_grid[i, j] == 1)
+        neighbours = self.neighbours(i, j)
+
+        if alive:
+            next_state = not (neighbours < 2 or neighbours > 3)
         else:
-            output += item + "\n"
-    return output
+            next_state = (neighbours == 3)
 
-def printMtrx(mtrx):
-    clear()
-    for i, sublist in enumerate(mtrx):
-        printLine(sublist, 0, i)
-    sleep(TIME_DELAY)
+        self.future_grid[i, j] = next_state
 
-def nextMove(mtrx):
-    newMtrx = deepcopy(mtrx)
 
-    for i, line in enumerate(newMtrx):
+def clear_screen():
+    system('cls' if name == 'nt' else 'clear')
 
-        iMin = i - 1
-        if iMin < 0:
-            iMin = SIZE - 1
 
-        iPlu = i + 1        
-        if iPlu > SIZE - 1:
-            iPlu = 0
+def query_user():
+    print """Oh holy ruler, welcome to \n
+        #####  #####  #####  ##   [v.02]
+        ##     ##     ##  #  ##
+        ##     ## ##  ##  #  ##
+        #####  #####  #####  ######"""
 
-        for j, item in enumerate(line):
-            neighbours = 0
+    grid_size = int(raw_input('Please input the size of The Garden of Eden [positive integer]: '))
+    chance_of_life = float(raw_input('Please input the chances of initial life [positive double between 0-1 ]: '))
+    evolve_timestep = float(
+        raw_input('Please specify the time period which life should develop by [positive double]: '))
 
-            jMin = j - 1
-            if jMin < 0:
-                jMin = SIZE - 1
+    return grid_size, chance_of_life, evolve_timestep
 
-            jPlu = j + 1
-            if jPlu > SIZE - 1:
-                jPlu = 0
 
-            neib = [("mtrx["+str(iMin)+"]["+str(jMin)+"]", mtrx[iMin][jMin]), ("mtrx["+str(iMin)+"]["+str(j)+"]", mtrx[iMin][j]),  ("mtrx["+str(iMin)+"]["+str(jPlu)+"]", mtrx[iMin][jPlu]),\
-                    ("mtrx["+str(i)+"]["+str(jMin)+"]", mtrx[i][jMin]), ("mtrx["+str(i)+"]["+str(jPlu)+"]", mtrx[i][jPlu]),\
-                    ("mtrx["+str(iPlu)+"]["+str(jMin)+"]", mtrx[iPlu][jMin]), ("mtrx["+str(iPlu)+"]["+str(j)+"]", mtrx[iPlu][j]), ("mtrx["+str(iPlu)+"]["+str(jPlu)+"]",mtrx[iPlu][jPlu])]
+if __name__ == "__main__":
+    size, my_chance, timestep = query_user()
 
-            neighbours = mtrx[iMin][jMin] + mtrx[iMin][j] + mtrx[iMin][jPlu] + mtrx[i][jMin] + mtrx[i][jPlu] + mtrx[iPlu][jMin] + mtrx[iPlu][j] + mtrx[iPlu][jPlu]
-            
-            if item == 1 and neighbours < 2:
-                newMtrx[i][j] = 0
-            elif item == 1 and (neighbours == 2 or neighbours == 3):
-                newMtrx[i][j] = 1
-            elif item == 1 and neighbours > 3:
-                newMtrx[i][j] = 0
-            elif item == 0 and neighbours == 3:
-                newMtrx[i][j] = 1
+    gol = GameOfLife(size, size)
+    gol.randomize(my_chance)
 
-    return newMtrx
+    # # Glider
+    # gol.current_grid[0, 1] = 1
+    # gol.current_grid[1, 2] = 1
+    # gol.current_grid[2, 0] = 1
+    # gol.current_grid[2, 1] = 1
+    # gol.current_grid[2, 2] = 1
 
-smMtrx = rndmMtrx(SIZE)
+    while True:
+        print gol.current_grid
 
-while 1:
-    printMtrx(smMtrx)
-    smMtrx = nextMove(smMtrx)
+        gol.evolve()
+
+        sleep(timestep)
+        clear_screen()
